d733bfdb7e7ef4f677b57ef144e795748a828358
alexisveloso25-png <alexis.veloso25@gmail.com>
2026-01-05 15:51:28 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -38,8 +38,9 @@ def histogram():
   
 @app.route('/extract-minutes/<date_string>')
 def extract_minutes(date_string):
-    date_object = datetime.strptime(date_string, '%Y-%m-%dT%H:%M:%SZ')
-    return jsonify({'minutes': date_object.minute})
+        date_object = datetime.strptime(date_string, '%Y-%m-%dT%H:%M:%SZ')
+        minutes = date_object.minute
+        return jsonify({'minutes': minutes})
 
 @app.route('/commits/')
 def commits():
