f7e8ba97626462e66ad7f7e1a3be62264bedadd0
alexisveloso25-png <alexis.veloso25@gmail.com>
2026-01-05 15:32:01 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -35,6 +35,12 @@ def mongraphique():
 def histogram():
     return render_template("histogram.html")
 
+@app.route('/extract-minutes/<date_string>')
+def extract_minutes(date_string):
+    date_object = datetime.strptime(date_string, '%Y-%m-%dT%H:%M:%SZ')
+    minutes = date_object.minute
+    return jsonify({'minutes': minutes})
+
 @app.route('/commits/')
 def commits():
     url = "https://api.github.com/repos/OpenRSI/5MCSI_Metriques/commits"
