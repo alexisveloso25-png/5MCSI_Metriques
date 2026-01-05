6cecb56dfe6881367e4aad2866712847702caea5
alexisveloso25-png <alexis.veloso25@gmail.com>
2026-01-05 14:42:58 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -30,6 +30,10 @@ def hello_world():
 @app.route("/rapport/")
 def mongraphique():
     return render_template("graphique.html")
+
+@app.route("histogramme/")
+def histogram():
+  return render_template("histogram.html")
   
 if __name__ == "__main__":
   app.run(debug=True)
