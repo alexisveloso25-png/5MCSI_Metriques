f94e2e55bc01e68f071cf39217c5023592e57b39
alexisveloso25-png <alexis.veloso25@gmail.com>
2026-01-05 15:01:10 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -32,7 +32,7 @@ def mongraphique():
     return render_template("graphique.html")
 
 @app.route("/histogramme/")
-def mongraphique():
+def histogram():
     return render_template("histogram.html")
 
 
